103f18dbfd1cc1c26b6b935cd4902655eb09a9bf
moses ngige <moses32ngigz@gmail.com>
2022-06-24 23:02:01 +0300

    changes to html tag content

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
 
-
 document.addEventListener("DOMContentLoaded", () => {
-  const newTaskPriority = document.getElementById("new-task-priority");
-  const newTaskUl = document.getElementById("tasks");
+  
+});
+document.addEventListener("DOMContentLoaded", () => {
+  //grab all the necessary DOM elements
+
+  //form and relevant input fields
   const newTaskForm = document.getElementById("create-task-form");
   const newTaskDescription = document.getElementById("new-task-description");
-    
-    newTaskForm.addEventListener("submit", createNewTask);
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  //ul where new tasks will live on the DOM
+  const newTaskUl = document.getElementById("tasks");
+
+  //attach event listeners
+  newTaskForm.addEventListener("submit", createNewTask);
+
 });
-const appendNewTask = task => {
-  document.getElementById("tasks").appendChild(task);
-};
 
 const createNewTask = event => {
   event.preventDefault();
-  const newTask = document.createElement("li");
+  //stop form from trying to submit
   const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
   newTask.innerText = newTaskDescription.value;
+
   appendNewTask(newTask);
   event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
 };
